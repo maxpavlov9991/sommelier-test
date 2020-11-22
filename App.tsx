c6ed5f7fd64f6df9400347cbf0c216cf9ae2e255
m.pavlov <m.pavlov@okko.tv>
2020-11-22 21:19:25 +0300

    added sommelier npm package with template

--- App.tsx
+++ App.tsx
@@ -1,12 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import {
+  H1,
+  H2,
+  H3,
+  H4,
+  H5,
+  H6,
+  Text,
+  Header,
+  Image,
+  Button,
+  Box,
+  StarsRating
+} from 'sommelier'
 
 export default function App() {
+  const [counter, setCounter] = useState(0)
+
+  const buttonPressHandler = () => {
+    setCounter(counter + 1)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <H1>H1</H1>
+      <H2>H2</H2>
+      <H3>H3</H3>
+      <H4>H4</H4>
+      <H5>H5</H5>
+      <H6>H6</H6>
+      <Text>Text</Text>
+      <Header>Header</Header>
+      <Image image='https://reactnative.dev/img/tiny_logo.png'></Image>
+      <Box>
+        <Button onPress={buttonPressHandler}>{'+ 1'}</Button>
+        <Text>{counter}</Text>
+      </Box>
+      <StarsRating rating={5} score={3}/>
     </View>
   );
 }
@@ -14,6 +45,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
