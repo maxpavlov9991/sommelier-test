dd51ed541398ad3db7ab2d02dbf9f43330762337
m.pavlov <m.pavlov@okko.tv>
2020-12-08 20:04:28 +0300

    fix #9999

--- App.tsx
+++ App.tsx
@@ -12,8 +12,18 @@ import {
   Image,
   Button,
   Box,
-  StarsRating
+  StarsRating,
+  Switch,
+  RadioButton,
+  SearchInput,
+  InputPrice,
+  Navbar,
+  Item
+
 } from 'sommelier'
+
+import { theme } from 'sommelier/App/theme'
+import { ThemeProvider } from 'styled-components/native'
 
 export default function App() {
   const [counter, setCounter] = useState(0)
@@ -23,32 +33,33 @@ export default function App() {
   }
 
   return (
-    <View style={styles.container}>
-      <H1>H1</H1>
-      <H2>H2</H2>
-      <H3>H3</H3>
-      <H4>H4</H4>
-      <H5>H5</H5>
-      <H6>H6</H6>
-      <Text>Text</Text>
-      <Header size='h1'>Header</Header>
-      <Image image='https://reactnative.dev/img/tiny_logo.png'></Image>
-      <Box>
-        <Text>{counter}</Text>
-      </Box>
-      <Button icon={''} onPress={buttonPressHandler} text={'+ 1'}/>
-      {counter}
-      <StarsRating rating={5} score={3}/>
+    <View>
+      <ThemeProvider theme={theme}>
+        <H1>H1</H1>
+        <H2>H2</H2>
+        <H3>H3</H3>
+        <View>
+          <StarsRating rating={5} score={3}/>
+        </View>
+        <H4>H4</H4>
+        <H5>H5</H5>
+        <InputPrice placeholder='Placeholder' value='500 рубасов'></InputPrice>
+        <H6>H6</H6>
+        <SearchInput onChange={() => console.log('search input changed')}></SearchInput>
+        <Switch value />
+        <Switch value switchType='heart'/>
+        <RadioButton checked ></RadioButton>
+        <Text>Text</Text>
+        <Header size='h1'>Header</Header>
+        <Image image={'https://i.ibb.co/PNTRVt0/shrek.png'} width={1000} height={400}/>
+        <Image image={'https://i.ibb.co/PNTRVt0/shrek.png'} width={1000} height={400}/>
+        <Button icon={''} onPress={buttonPressHandler} text={'+ 1'}/>
+        <Navbar>
+          <Item text={'Item1'} onPress={() => console.log('NavbarItemPressed 1')}/>
+          <Item text={'Item2'} onPress={() => console.log('NavbarItemPressed 2')}>Item 2</Item>
+          <Item text={'Item3'} onPress={() => console.log('NavbarItemPressed 3')}>Item 3</Item>
+        </Navbar>
+      </ThemeProvider>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: 'column',
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
